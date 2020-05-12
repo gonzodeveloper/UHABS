64ccd8709f188bc55ece0146f306d2af23700e11
andto94 <andto94@gmail>
2020-05-11 18:59:20 -1000

    logic

--- gps_sim.py
+++ gps_sim.py
@@ -12,7 +12,30 @@ TIMESTEP = 10
 
 def caluclate_position(currents_map, bearing, position):
     # Code here
+    """
+    Calculates position based off currents map, bearing, and propulsion of the module
+    :param currents_map: u(y, x), v(y, x) 2Dd component arrays for current values; np.dstack
+    :param position: (y, x) present position of module; tuple
+    :param bearing: angle defined from East for module heading; float
+    :return: (y, x) new position of module; tuple
+    """
+    # Grab module position in grid
+    n, m = position
 
+    # Grab v and u arrays from current maps
+    v_array = currents_map[::, ::, 0]
+    u_array = currents_map[::, ::, 1]
 
-    return (y, x)
+    # Calculate velocity components of module
+    boat_x_velocity = PROPULSION * np.cos(bearing)
+    boat_y_velocity = PROPULSION * np.sin(bearing)
+
+    # Calculate new position of module
+    new_position = [n + (v_array[n] + boat_y_velocity) * timestep,
+                    m + (u_array[m] + boat_x_velocity) * timestep]
+
+    # Round values of position tuple to fix to a grid vertex
+    new_position = np.rint(new_position)
+
+    return new_position
 
---
+++ logic.py
@@ -0,0 +1,48 @@
+from .nav_sim import transform_map, next_bearing
+from .gps_sim import calculate_positions
+from .comms import send, recv
+from .driver import set_bearing
+
+import numpy as np
+NEW_BEARING = '0'
+NEW_MAP = '1'
+telemetry = '5'
+
+
+def control(conn, bearing, pos, dest, map):
+    """
+    Constantly sends current position, path, and telemetry to ground station
+    Receives manual input for bearings and new current maps
+    :param conn:
+    :param bearing: angle defined with East as 0; float
+    :param pos: (y, x) coordinate of module's present position; tuple
+    :param dest: (y, x) coordinate of module's destination; tuple
+    :param map: u(y, x), v(y, x) 2D component arrays for current values; np.dstack
+    :return:
+    """
+    # Transform the present currents map to a graph
+    graph = transform_map(map)
+    while True:
+
+        # Get flags of received signals
+        flag, data = recv(conn)
+
+        # If received signal is a new bearing, set new bearing to match.
+        if flag == NEW_BEARING:
+            bearing = data
+            set_bearing(bearing)
+            continue
+
+        # If received signal is a new currents map, set new map to match
+        elif flag == NEW_MAP:
+            map = data
+            graph = transform_map(map)
+
+        # Recalculate position and bearing based off updated current map and bearing
+        pos = calculate_positions(map, bearing, pos)
+        bearing = next_bearing(graph, pos, dest)
+
+        # Send current bearing, position, and telemetry to ground station
+        send(bearing, pos, telemetry)
+
+    return
