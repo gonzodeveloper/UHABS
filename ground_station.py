fc630f667e52e3d3ab0fe9f7b1da67ecc47e5e20
andto94 <andto94@gmail.com>
2020-05-18 13:52:17 -1000

    listener/transmitter fixes + testing

--- ground_station.py
+++ ground_station.py
@@ -23,31 +23,47 @@ def main(config):
 
 	# Initialize communications
 	ground_station = config['gs_addr']
+	boat = config['boat_addr']
 	port_list = config['ports']
 
-	path_transmitter = Listener(ground_station, port_list['nav_path'])
-	telem_transmitter = Listener(ground_station, port_list['telem'])
-	gps_transmitter = Listener(ground_station, port_list['gps'])
+	path_transmitter = Listener(boat, port_list['nav_path'])
+	telem_transmitter = Listener(boat, port_list['telem'])
+	gps_transmitter = Listener(boat, port_list['gps'])
 
 	instr_az_comms  = Transmistter(ground_station, port_list['manual_az'])
 	instr_prop_comms = Transmistter(ground_station, port_list['manual_prop'])
 	new_map_listener = Transmistter(ground_station, port_list['maps'])
 
-def controls(comms)
+def controls(comms):
+
+	# Initialize
 	quit = False
 	while not quit:
+
+		# Prompt for additional instructions
 		choice = input("Please input instruction type (AZ, PROP, MAP): ")
+
+		# Receive azimuth input and send
 		if choice == "AZ":
 			new_az = input("Please input new azimuth: ")
 			new_az = np.float32(new_az)
-			comms.send(new_az)
+			# comms.send(new_az)
+			print("You just sent {}.".format('new_az'))
+
+		# Receive propulsion input and send
 		if choice == "PROP":
 			new_prop = input("Please input new propulsion: ")
 			new_prop = np.float32(new_prop)
-			comms.send(new_prop)
+			# comms.send(new_prop)
+			print("You just sent {}.".format('new_prop'))
+
+		# Receive new map file input and send
 		if choice == "MAP":
 			new_map = input("Please input file location for new map: ")
 			new_map_array = read_netcdf(new_map)
-			comms.send(new_map_array)
+			# comms.send(new_map_array)
+			print("You just sent {}.".format('new_map'))
+
+		# Quit controls
 		if choice == "quit":
 			quit == True
